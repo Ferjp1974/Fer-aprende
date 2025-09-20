620bb4a80c965b57511b00973d5b06a1a23d0d5d
fer <tormen_fer@hotmail.com>
2025-09-19 21:02:57 -0300

    algo mas

---
+++ scripts.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+const heading = document.createElement("h1");   heading.textContent = "Hola Fer!";
+document.body.appendChild(heading);
+
+const paragraph1 = document.createElement("p");
+paragraph1.textContent = "Bienvenido a tu primer proyecto web.";
+document.body.appendChild(paragraph1);
+
+const paragraph2 = document.createElement("p");
+paragraph2.textContent = "Esto es lo primero que has escrito.";
+document.body.appendChild(paragraph2);
+});
